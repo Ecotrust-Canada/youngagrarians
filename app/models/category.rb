e39c13ed77655e10dc2189c069eae3ddb02c9008
Adam Palmblad <adam.palmblad@teampages.com>
2016-04-24 23:17:57 -0700

    categories should require a name.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ActiveRecord::Base
   has_many :locations
   has_many :subcategories
   attr_accessible :name
+  validates :name, presence: true
 
   def as_json(_options)
     super include: :subcategories
