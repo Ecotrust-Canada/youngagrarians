a6a00d5f215d098bfd2727c9da0d491638e9e3ed
Adam Palmblad <adam.palmblad@teampages.com>
2016-05-22 16:23:44 -0700

    Fix tests for sitemap.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -7,4 +7,10 @@ class HomeControllerTest < ActionController::TestCase
     get :sitemap, format: 'xml'
     assert_response :success
   end
+  # ------------------------------------------------------- test_viewing_sitemap
+  def test_viewing_sitemap_with_top_level_categories
+    3.times{ FactoryGirl.create( :nested_category, parent: nil ) }
+    get :sitemap, format: 'xml'
+    assert_response :success
+  end
 end
