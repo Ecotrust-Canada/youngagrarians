6e9aea3c2dbbadeb642e45582a7a85830e082f44
Clark Van Oyen <clark@ecotrust.ca>
2016-06-09 21:15:12 -0400

    stage branch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,14 @@
 class HomeController < ApplicationController
+  after_action :allow_iframe
   def index
     render layout: 'basic'
   end
   def map
-    render layout: false, embed: @embed
+    render layout: false
+  end
+
+private
+  def allow_iframe
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
   end
 end
