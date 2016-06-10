37bf5def5e91b6cf40ec6704377f9a00acf656b5
Adam Palmblad <adam.palmblad@teampages.com>
2016-06-09 20:04:34 -0700

    Fix N query problem.
    TODO: Watch out for recurrence as we play with categories.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -32,7 +32,7 @@ class LocationsController < ApplicationController
         end
 
         scope = apply_search_scope( scope ) if params[:q].present?
-        @locations = scope.approved.currently_shown.includes( category_tags: { nested_category: :parent } ).order( 'id' )
+        @locations = scope.approved.currently_shown.includes( nested_categories: :parent ).order( 'id' )
       end
     end
   end
