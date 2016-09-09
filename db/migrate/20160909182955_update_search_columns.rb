bfa634c92c12b9a10ff9905ed010b5c1aea9a954
Oleg Matvejev <omatveje@gmail.com>
2016-09-09 15:56:29 -0700

    Proper migration for generating search values
    - The old one didn't actually work. You can't have multiple triggers like that
      for different languages because it would just overwrite it.

--- db/migrate/20160909182955_update_search_columns.rb
+++ db/migrate/20160909182955_update_search_columns.rb
@@ -1,20 +1,14 @@
 class UpdateSearchColumns < ActiveRecord::Migration
   def up
     if postgres?
-      #execute( "DROP TRIGGER tsvectorupdate ON locations")
-      execute( "CREATE TRIGGER tsvectorupdate_english
+      execute( "DROP TRIGGER tsvectorupdate ON locations")
+
+      execute( "CREATE TRIGGER tsvectorupdate
                 BEFORE INSERT OR UPDATE
                 ON locations
                 FOR EACH ROW
                 EXECUTE PROCEDURE
                   tsvector_update_trigger(search, 'pg_catalog.english', name, content, description, resource_type, street_address, bioregion)" )
-
-      execute( "CREATE TRIGGER tsvectorupdate_french
-                BEFORE INSERT OR UPDATE
-                ON locations
-                FOR EACH ROW
-                EXECUTE PROCEDURE
-                  tsvector_update_trigger(search, 'pg_catalog.french', name, content, description, street_address, bioregion)" )
 
       execute( "UPDATE locations SET search =
                   to_tsvector( 'english', COALESCE( name, '' ) ) ||
@@ -22,12 +16,7 @@ class UpdateSearchColumns < ActiveRecord::Migration
                   to_tsvector( 'english', COALESCE( description, '' ) ) ||
                   to_tsvector( 'english', COALESCE( resource_type, '' ) ) ||
                   to_tsvector( 'english', COALESCE( street_address, '' ) ) ||
-                  to_tsvector( 'english', COALESCE( bioregion, '' ) ) ||
-                  to_tsvector( 'french', COALESCE( name, '' ) ) ||
-                  to_tsvector( 'french', COALESCE( content, '' ) ) || 
-                  to_tsvector( 'french', COALESCE( description, '' ) ) ||
-                  to_tsvector( 'french', COALESCE( street_address, '' ) ) ||
-                  to_tsvector( 'french', COALESCE( bioregion, '' ) )" ) 
+                  to_tsvector( 'english', COALESCE( bioregion, '' ) )" ) 
     end
   end
   # ------------------------------------------------------------------ postgres?
@@ -38,7 +27,7 @@ class UpdateSearchColumns < ActiveRecord::Migration
   end
 
   def down
-    execute( "DROP TRIGGER IF EXISTS tsvectorupdate ON locations")
+    execute( "DROP TRIGGER tsvectorupdate ON locations")
     execute( "CREATE TRIGGER tsvectorupdate
                 BEFORE INSERT OR UPDATE
                 ON locations
