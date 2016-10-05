2800b3922017a04de830969d1387436abda31d3a
Clark Van Oyen <clark@countable.ca>
2016-10-04 19:58:13 -0700

    ontario bug

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -287,7 +287,7 @@ class LocationsController < ApplicationController
         scope.where( "search @@ (#{clause}) OR locations.id IN ( ? )", *args )
       else
         if args.include? "ON" # ON is a reserved stop word in pg fulltext search apparently! Let's have a special case:
-          args << "Ontario"
+          args = ["Ontario"]
           scope.where( "locations.province = 'ON' OR search @@ (#{clause})", *args)
         else
           scope.where( "search @@ (#{clause})", *args )
@@ -304,7 +304,7 @@ class LocationsController < ApplicationController
 
   # -------------------------------------------------- generate_tokens
   def generate_tokens(query)
-    #query = query.gsub(/Ontario/i, 'ON')
+    query = query.gsub(/Ontario/i, 'ON')
     query = query.gsub(/Quebec/i, 'QC')
     query = query.gsub(/Nova Scotia/i, 'NS')
     query = query.gsub(/New Brunswick/i, 'NB')
