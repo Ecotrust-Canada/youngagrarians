09fde31f1b67bc07f19c870e7dacab9babd8a501
Oleg Matvejev <omatveje@gmail.com>
2017-06-09 17:31:44 -0700

    We shouldn't notify the user if it is a spam comment either

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,14 +8,14 @@ class CommentsController < ApplicationController
     Rails.logger.info "+ Adding comment with akismet reponse: #{@comment.akismet_response}"
     @comment.save
 
+    # Return early if the comment is a spam comment, as we don't want to notify either admin or the registered user about it
+    return redirect_to location_url( @location ) if @comment.is_spam
+
     if @location.email.present? || @location.account
       UserMailer.new_comment( @comment ).deliver_now
     end
 
-    # Only send a notification to admin if it is not a spam comment
-    unless @comment.is_spam
-      UserMailer.new_comment_for_admin( @comment ).deliver_now
-    end
+    UserMailer.new_comment_for_admin( @comment ).deliver_now
 
     redirect_to location_url( @location )
   end
